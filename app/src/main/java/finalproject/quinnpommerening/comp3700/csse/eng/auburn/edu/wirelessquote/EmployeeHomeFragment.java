c5113a4d1ed8c76e64e55e2eebdf1ae64a8fa01c
Quinn Pommerening <qpommer0@gmail.com>
2015-04-08 21:24:27 -0500

    Added a lot of different features.

--- app/src/main/java/finalproject/quinnpommerening/comp3700/csse/eng/auburn/edu/wirelessquote/EmployeeHomeFragment.java
+++ app/src/main/java/finalproject/quinnpommerening/comp3700/csse/eng/auburn/edu/wirelessquote/EmployeeHomeFragment.java
@@ -1,32 +1,56 @@
 package finalproject.quinnpommerening.comp3700.csse.eng.auburn.edu.wirelessquote;
 
-import android.app.Activity;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
 
  */
 public class EmployeeHomeFragment extends Fragment {
+        private Button mCreateNewQuote;
+        private Button mManageQuote;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
-        }
+        
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_employee_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_employee_home, container, false);
+        mCreateNewQuote = (Button)v.findViewById(R.id.create_quote_button);
+        mManageQuote = (Button) v.findViewById(R.id.manage_quote_button);
+
+        mCreateNewQuote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment display = new CustomerInformationFragment();
+                getFragmentManager().beginTransaction()
+                        .addToBackStack("fragment")
+                        .replace(R.id.fragment_container, display, "display")
+                        .commit();
+            }
+        });
+
+        mManageQuote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment display = new ManageQuoteFragment();
+                getFragmentManager().beginTransaction()
+                        .addToBackStack("fragment")
+                        .replace(R.id.fragment_container, display, "display")
+                        .commit();
+            }
+        });
+        return v;
     }
 
 
